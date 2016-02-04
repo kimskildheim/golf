c9d55d17a4c558651d06df9fce73e90f600ecf9f
kim skildheim <kimskildheim@gmail.com>
2016-02-04 16:31:19 +0100

    Spring app up an almost running

---
+++ src/main/java/no/kds/GolfApplication.java
@@ -0,0 +1,12 @@
+package no.kds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfApplication.class, args);
+	}
+}
